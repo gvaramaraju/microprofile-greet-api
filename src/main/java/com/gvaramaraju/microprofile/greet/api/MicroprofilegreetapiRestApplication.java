45c3cee6335dc769b259ff2f3f4083f62cb636f0
gvaramaraju <ramaraju.gva@gmail.com>
2022-06-25 11:05:24 +0530

    Initial commit

---
+++ src/main/java/com/gvaramaraju/microprofile/greet/api/MicroprofilegreetapiRestApplication.java
@@ -0,0 +1,11 @@
+package com.gvaramaraju.microprofile.greet.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class MicroprofilegreetapiRestApplication extends Application {
+}
